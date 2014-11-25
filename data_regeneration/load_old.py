06788cb6a0af5373b270c51d72253ad05ad91906
Dara Adib <dadib@synthicity.com>
2014-11-24 19:29:40 -0800

    Add shapefile loading script for old northern counties

---
+++ data_regeneration/load_old.py
@@ -0,0 +1,22 @@
+import logging
+
+from spandex import TableLoader
+from spandex.io import logger
+from spandex.spatialtoolz import conform_srids
+
+
+logger.setLevel(logging.INFO)
+
+
+shapefiles = {'staging.old_cnc': 'contra_costa.shp',
+              'staging.old_nap': 'napa.shp',
+              'staging.old_son': 'sonoma.shp'}
+
+
+# Load shapefiles specified above to the project database.
+loader = TableLoader()
+loader.load_shp_map(shapefiles)
+
+# Fix invalid geometries and reproject.
+staging = loader.tables.staging
+conform_srids(loader.srid, schema=staging, fix=True)
