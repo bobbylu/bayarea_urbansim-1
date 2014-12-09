9122a03462e8ad2125da262f700cecc48b5d154b
Dara Adib <dadib@synthicity.com>
2014-12-08 17:03:33 -0800

    Fix data summaries SQL

--- data_regeneration/data_summaries.sql
+++ data_regeneration/data_summaries.sql
@@ -5,20 +5,20 @@ GROUP BY county_id
 ORDER BY county_id;
 
 -- Report sum of building_sqft by county.
-SELECT   county_id, sum(building_sqft) AS building_sqft,
+SELECT   county_id, sum(building_sqft) AS building_sqft
 FROM     parcels
 GROUP BY county_id
 ORDER BY county_id;
 
 -- Report sum of non_residential_sqft by county.
-SELECT   county_id, sum(non_residential_sqft) AS non_residential_sqft,
+SELECT   county_id, sum(non_residential_sqft) AS non_residential_sqft
 FROM     parcels
 GROUP BY county_id
 ORDER BY county_id;
 
 
 -- Report sum of residential_units by county.
-SELECT   county_id, sum(residential_units) AS residential_units,
+SELECT   county_id, sum(residential_units) AS residential_units
 FROM     parcels
 GROUP BY county_id
 ORDER BY county_id;
